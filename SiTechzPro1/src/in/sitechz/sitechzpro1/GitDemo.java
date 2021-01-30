901a9ee953fe2f0e5bd05806c2817473a9c4bcf1
Sreekandan K <sreekandank@users.noreply.github.com>
2021-01-30 23:19:06 +0530

    Git Push second time

--- SiTechzPro1/src/in/sitechz/sitechzpro1/GitDemo.java
+++ SiTechzPro1/src/in/sitechz/sitechzpro1/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
 	public static void main(String args[]) {
 		System.out.println("Welcome to SiTechz");
 		System.out.println("Git Pull Demo");
+		System.out.println("Git Pull Demo Success");
 	}
 }
