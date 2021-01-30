559b1bc46932869e4997c61837e9f3c615a93c00
SiTechZ <78252437+SiTechz@users.noreply.github.com>
2021-01-30 23:17:01 +0530

    Update GitDemo.java
    Git Pull Demo

--- SiTechzPro1/src/in/sitechz/sitechzpro1/GitDemo.java
+++ SiTechzPro1/src/in/sitechz/sitechzpro1/GitDemo.java
@@ -3,5 +3,6 @@ package in.sitechz.sitechzpro1;
 public class GitDemo {
 	public static void main(String args[]) {
 		System.out.println("Welcome to SiTechz");
+		System.out.println("Git Pull Demo");
 	}
 }
