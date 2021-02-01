2eb4a1eb44d9c7f6a1175f9e54b49f3dc9538ec5
SiTechz Developer <sitechz@users.noreply.github.com>
2021-02-01 20:02:02 +0530

    code change

--- SiTechzPro1/src/in/sitechz/sitechzpro1/test1.java
+++ SiTechzPro1/src/in/sitechz/sitechzpro1/test1.java
@@ -2,7 +2,7 @@ package in.sitechz.sitechzpro1;
 
 public class test1 {
 	public static void main(String args[]) {
-		String msg = "Welcome to SiTechz";
+		String message = "Welcome to SiTechz";
 		
 	}
 
