607ac34505f3c5ba003923e5752d547f1b18949c
SiTechz Developer <sitechz@users.noreply.github.com>
2021-02-02 15:35:21 +0530

    demo for new branch

--- SiTechzPro1/src/in/sitechz/sitechzpro1/test1.java
+++ SiTechzPro1/src/in/sitechz/sitechzpro1/test1.java
@@ -3,7 +3,6 @@ package in.sitechz.sitechzpro1;
 public class test1 {
 	public static void main(String args[]) {
 		String message = "Welcome to SiTechz";
-		
+		System.out.println(message);	
 	}
-
 }
